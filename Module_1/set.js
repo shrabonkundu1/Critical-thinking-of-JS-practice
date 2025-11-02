8eda1ea76115113d8e6c9bd1c206461b513f2a07
Shrabon Kundu <shrabonkundu@gmail.com>
2025-11-02 12:59:54 +0600

    set use kore duplication removed

--- Module_1/set.js
+++ Module_1/set.js
@@ -10,3 +10,55 @@ const setArr = [...set];
 console.log(setArr)
 
 //***set er kaj hocce data er duplication remove kora ata use hoi data search er khetre  */
+
+
+// data khojar jonno set.has("jeet")
+
+//  data delete er jonno set.delete("Rumpa");
+
+
+
+// just foreach kore duplicate remove ---
+
+const removeDuplicateArr = (arr) => {
+    const newArr = [];
+
+    arr.forEach(element => {
+       if(!newArr.includes(element)){
+        newArr.push(element);
+       } 
+    });
+
+    return newArr;
+
+}
+
+console.log(removeDuplicateArr(arr))
+
+
+// set use kore duplicate removef--------
+
+const removeDupSet = (arr) => {
+    const set = new Set(arr);
+    
+    const removeArr = [...set];
+
+    return removeArr;
+}
+
+console.log(removeDupSet(arr))
+
+
+
+// athoba
+
+
+const removeDupArr2 = (arr) =>{
+
+    const set = new Set(arr);
+
+    return Array.from(set);
+
+}
+
+console.log("removeDupArr2",removeDupArr2(arr))
