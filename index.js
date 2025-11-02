0250b7a3042ea1c17651ca0641a0ef8323286028
Shrabon Kundu <shrabonkundu@gmail.com>
2025-11-02 11:54:01 +0600

    Data map kore time complexity nirnoi

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const startTime = performance.now();
 for(let i = 0; i < 50 ; i++){
-    console.log(i)
+    // console.log(i)
 }
 
 const endTime = performance.now();
 
-console.log(`This function took time ${endTime - startTime} ms`)
+// console.log(`This function took time ${endTime - startTime} ms`)
 
 
-// option 2 : 
+// option 2 : -----
 
-console.time('task');
+// console.time('task');
 
 for (let i = 0; i < 5000 ; i++){
-    console.log(i);
+    // console.log(i);
 }
 
-console.timeEnd('task')
+// console.timeEnd('task')
 
 
-// Abstract way 
+// Abstract way -------------
 
 const firstArray = [];
 const secondArray = [];
@@ -32,5 +32,23 @@ for (let i = 0; i < 6000000; i++){
 }
 
 
-console.log("first array",firstArray.length)
-console.log("second array",secondArray.length)
+// console.log("first array",firstArray.length)
+// console.log("second array",secondArray.length);
+
+
+// data gula map kore time complexity ber korbo------------
+
+console.time("map1");
+
+const firstArrayList = firstArray.map((number) => ({userID: number}));
+
+console.timeEnd("map1");
+
+console.time("map2");
+
+const secondArrayList = secondArray.map((number) => ({userID: number}));
+
+console.timeEnd("map2");
+
+
+
