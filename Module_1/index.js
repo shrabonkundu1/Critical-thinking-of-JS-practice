67ed82bfc37633ee1eafa4514d0d3785338e32a4
Shrabon Kundu <shrabonkundu@gmail.com>
2025-11-02 12:30:50 +0600

    Set data structure er usecase

---
+++ Module_1/index.js
@@ -0,0 +1,70 @@
+const startTime = performance.now();
+for(let i = 0; i < 50 ; i++){
+    // console.log(i)
+}
+
+const endTime = performance.now();
+
+// console.log(`This function took time ${endTime - startTime} ms`)
+
+
+// option 2 : -----
+
+// console.time('task');
+
+for (let i = 0; i < 5000 ; i++){
+    // console.log(i);
+}
+
+// console.timeEnd('task')
+
+
+// Abstract way -------------
+
+const firstArray = [];
+const secondArray = [];
+
+for (let i = 0; i < 6000000; i++){
+    if( i < 3000000 ){
+        firstArray.push(i);
+    }
+    secondArray.push(i);
+}
+
+
+// console.log("first array",firstArray.length)
+// console.log("second array",secondArray.length);
+
+
+// data gula map kore time complexity ber korbo------------
+
+console.time("map1");
+
+const firstArrayList = firstArray.map((number) => ({userID: number}));
+
+console.timeEnd("map1");
+
+console.time("map2");
+
+const secondArrayList = secondArray.map((number) => ({userID: number}));
+
+console.timeEnd("map2");
+
+
+
+//  -------------data find kore time complexity nirnoi------------
+
+console.time("find");
+
+const user = secondArrayList.find((user) => user.userID === 50000000);
+
+console.timeEnd("find")
+
+
+///-----------------------data includes kore index diye time complexity nirnoi---------
+
+console.time("index");
+
+const user2 = secondArrayList.includes[500000000];
+
+console.timeEnd("index")            //so data khojar dorkar hole find() na caliye includes[] calale O(1) hoi
---
+++ Module_1/set.js
@@ -0,0 +1,12 @@
+const arr = ["shrabon", "Rima", "Ritu", "Rumpa", "Rinta", "Rima", "jeet", "Shompa", "jeet", "shrabon"];
+
+const set = new Set(arr);
+
+console.log(set)
+
+const setArr = [...set];
+
+
+console.log(setArr)
+
+//***set er kaj hocce data er duplication remove kora ata use hoi data search er khetre  */
