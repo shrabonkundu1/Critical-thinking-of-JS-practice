e893c10e9d0a17e84ea2595b13fc1611b4f32b53
Shrabon Kundu <shrabonkundu@gmail.com>
2025-11-03 20:24:10 +0600

    Difference between object dot or square notation

---
+++ Module_2/object_map_practice.js
@@ -0,0 +1,16 @@
+const obj = {
+    nextLevel : {courseId : "level2"},
+
+    "programming hero" : {courseId : "level1"}
+}
+
+//  use object dot.notation
+console.log(obj.nextLevel)
+
+//-------out pur ----{ courseId: 'level2' }
+
+
+// use object square notation: 
+console.log(obj["programming hero"])
+
+//--------out put --{ courseId: 'level1' }
